a30b26edc5c498f4bedf38d00ee8cc6d4e2ec065
ayobamy <ayobamyahmed@gmail.com>
2022-05-18 12:56:38 -0700

    [feat]: #2 created shell main c file

---
+++ sshell.c
@@ -0,0 +1,26 @@
+#include "sshell.h"
+
+int main(void)
+{
+	char *line = NULL;
+	char **args = NULL;
+	int i = 0;
+
+	while (1)
+	{
+		line = _getline();
+		if (line == NULL)
+			return (0);
+
+		args = parsing(line);
+		if (args == NULL)
+			return (0);
+			
+		for (i = 0; args[i] != NULL; i++)
+			;
+		_fork(args, line, i);
+		free(line);
+		gridfree(args, i);
+	}
+	return (0);
+}
