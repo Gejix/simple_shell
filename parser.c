189eda71c85b28f879f74e5bcefd1fe52ad29941
ayobamy <ayobamyahmed@gmail.com>
2022-05-18 12:50:22 -0700

    [feat]: #14 created parser.c file

---
+++ parser.c
@@ -0,0 +1,72 @@
+#include "sshell.h"
+
+/**
+ * Counter - counts the number of lim found in the input
+ * @C: the input string;
+ * @lim: character to find inside the C string
+ * Return: number of characters found
+ */
+
+int Counter(char *C, char *lim)
+{
+	int i = 0, num = 0;
+	if (lim != NULL)
+	{
+		while (C && C[i] != '\0')
+		{
+			if (C[i] != lim[0])
+			{
+				if (C[i + 1] == lim[0] || C[i + 1] == '\0')
+					num++;
+			}
+			i++;
+		}
+	}
+	return (num);
+}
+
+/**
+ * parsing - create an array of pointers depending of the delimit characters
+ * @line: input of the user
+ * Return: an array of pointers of n size
+ */
+
+char **parsing(char *line)
+{
+	char *token = NULL, **p = NULL;
+	int length = 0, j = 0, i = 0, buffsize = 0;
+
+	if (line == NULL)
+		return (NULL);
+	buffsize = Counter(line, " ");
+	p = _calloc((buffsize + 1), sizeof(char *));
+	if (!p)
+	{
+		perror("No memory");
+		exit(EXIT_FAILURE);
+	}
+	/*store the token partition inside **p */
+	token = strtok(line, " \n");
+	while (token)
+	{
+		while (token[length] != '\0')
+			length++;
+		p[j] = _calloc((length + 1), sizeof(char));
+		if (p[j] == NULL)
+		{
+			gridfree(p, j);
+			perror("No memory");
+			exit(EXIT_FAILURE);
+		}
+		/*fill the pointer with the content of token*/
+		for (i = 0; i < length; i++)
+			p[j][i] = token[i];
+		
+		length = 0;
+		j++;
+		/*get the next element*/
+		token = strtok(NULL, " \n");
+	}
+	p[j] = NULL;
+	return (p);
+}
