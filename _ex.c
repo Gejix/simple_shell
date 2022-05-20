cb4f437aab574ea13906c804bd9a6448476a80df
Gejix <geraldjuwah@gmail.com>
2022-05-20 15:48:21 +0100

    [fix]: Updated exit function

--- _ex.c
+++ _ex.c
@@ -1,21 +1,23 @@
 #include "sshell.h"
-
 /**
  *type_exit - get the type of exit
  * @p: input user, array of pointers
  * @loop: counter of loops
- * @line: input user
+ * @li: input user
  * @i: number of pointers inside the array of pointers
  * @v: arguments in input
+ * @m: copy of environmental variables
+ * @e: number of elements in m
  */
-void type_exit(char **p, int loop, char *line, int i, char *v[])
+void type_exit(char **p, int loop, char *li, int i, char *v[], char **m, int e)
 {
 	unsigned int valor = 0, cont = 0, flag = 0;
 
 	if (p[1] == NULL || (p[1][0] == '0' && p[1][1] == '\0'))
 	{
-		free(line);
+		free(li);
 		gridfree(p, i);
+		gridfree(m, e);
 		exit(0);
 	}
 	else
@@ -38,8 +40,9 @@ void type_exit(char **p, int loop, char *line, int i, char *v[])
 			if (!(valor > INT_MAX))
 			{
 				valor = valor % 256;
-				free(line);
+				free(li);
 				gridfree(p, i);
+				gridfree(m, e);
 				exit(valor);
 			}
 			else
@@ -51,12 +54,14 @@ void type_exit(char **p, int loop, char *line, int i, char *v[])
  * _ex - finds if line input is exit therefore process termination
  * @p: input of user
  * @loop: loop counter
- * @line: input user
+ * @li: input user
  * @x: number of pointers inside array of pointers
  * @v: arguments in input
- * return: salida
+ * @m: copy of environmental variables
+ * @e: number of elements in m
+ * Return: -1 if there is no exit or 0 if there is the word exit
  */
-int _ex(char **p, int loop, char *line, int x, char *v[])
+int _ex(char **p, int loop, char *li, int x, char *v[], char **m, int e)
 {
 	char str[] = "exit";
 	int i, cont = 0, salida = -1;
@@ -76,7 +81,7 @@ int _ex(char **p, int loop, char *line, int x, char *v[])
 
 	if (cont == 5)
 	{
-		type_exit(p, loop, line, x, v);
+		type_exit(p, loop, li, x, v, m, e);
 		salida = 0;
 	}
 	else if (cont == 4)
