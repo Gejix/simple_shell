b51648d6193187e16167b8160cff3ee8fcc93d36
Gejix <geraldjuwah@gmail.com>
2022-05-20 15:47:21 +0100

    [fix]: Updated builtin error function

--- _errors.c
+++ _errors.c
@@ -34,14 +34,11 @@ void _errorhelp(char **p)
 	write(STDERR_FILENO, p[0], 4);
 	write(STDERR_FILENO, ": no help topics match '", 24);
 	write(STDERR_FILENO, p[1], _strlen(p[1]));
-	write(STDERR_FILENO, "'", 1);
-	write(STDERR_FILENO, ". Try 'help help' or 'man -k '", 30);
+	write(STDERR_FILENO, "'. Try 'help help' or 'man -k '", 31);
 	write(STDERR_FILENO, p[1], _strlen(p[1]));
-	write(STDERR_FILENO, "'", 1);
-	write(STDERR_FILENO, " or info '", 10);
+	write(STDERR_FILENO, "' or info '", 11);
 	write(STDERR_FILENO, p[1], _strlen(p[1]));
-	write(STDERR_FILENO, "'", 1);
-	write(STDERR_FILENO, "\n", 1);
+	write(STDERR_FILENO, "'\n", 2);
 }
 /**
  * _errorgarbage - writes the error
@@ -51,7 +48,17 @@ void _errorhelp(char **p)
 void _errorgarbage(char **p)
 {
 	write(STDERR_FILENO, p[0], _strlen(p[0]));
-	write(STDERR_FILENO, ": not found", 11);
+	write(STDERR_FILENO, ": not found\n", 13);
+}
+/**
+ * _errorenv - writes the error
+ * @p: input pointer
+ * Return: nothing.
+ */
+void _errorenv(char **p)
+{
+	write(STDERR_FILENO, p[0], _strlen(p[0]));
+	write(STDERR_FILENO, ": unable to add/rm variable ", 28);
+	write(STDERR_FILENO, p[1], _strlen(p[1]));
 	write(STDERR_FILENO, "\n", 1);
 }
-
